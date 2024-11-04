9be1750373848ad75b9f67b28cbf596c3b5d076b
ERR0RW0LF <finn.woma@gmail.com>
2024-11-04 15:50:53 +0100

    Implement OpenGL context creation and event loop; update main function for rendering

--- learn_gl_with_rust/src/main.rs
+++ learn_gl_with_rust/src/main.rs
@@ -1,3 +1,46 @@
+use glutin::event_loop::ControlFlow;
+use glutin::event::{WindowEvent, Event};
+use glutin::Api;
+use glutin::GlRequest;
+use glutin::ContextBuilder;
+use glutin::window::WindowBuilder;
+use glutin::event_loop::EventLoop;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().with_title("Learn OpenGL with Rust");
+    
+    let gl_context = ContextBuilder::new()
+        .with_gl(GlRequest::Specific(Api::OpenGl, (3, 3)))
+        .build_windowed(window, &event_loop)
+        .expect("Cannot create windowed context");
+    
+    let gl_context = unsafe {
+        gl_context
+            .make_current()
+            .expect("Failed to make context current")
+    };
+    
+    gl::load_with(|ptr| gl_context.get_proc_address(ptr) as *const _);
+    
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+    
+        match event {
+            Event::LoopDestroyed => (),
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+                WindowEvent::Resized(physical_size) => gl_context.resize(physical_size),
+            },
+            Event::RedrawRequested(_) => {
+                unsafe {
+                    gl::ClearColor(0.0, 0.0, 1.0, 1.0);
+                    gl::Clear(gl::COLOR_BUFFER_BIT);
+                }
+                gl_context.swap_buffers().unwrap();
+            }            
+            _ => (),
+        }
+    });
 }
